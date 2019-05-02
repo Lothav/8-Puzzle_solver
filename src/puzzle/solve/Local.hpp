f36d5dd69e4cbfd406a86bf052268fd634f586f8
Luiz0tavio <me@luizotav.io>
2019-05-02 14:58:51 -0300

    solve local class

---
+++ src/puzzle/solve/Local.hpp
@@ -0,0 +1,14 @@
+#ifndef INC_8_PUZZLE_SOLVER_LOCAL_HPP
+#define INC_8_PUZZLE_SOLVER_LOCAL_HPP
+namespace puzzle
+{
+    namespace solve
+    {
+        class Local
+        {
+
+        };
+    }
+}
+
+#endif //INC_8_PUZZLE_SOLVER_LOCAL_HPP
