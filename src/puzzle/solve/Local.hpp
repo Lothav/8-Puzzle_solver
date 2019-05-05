c7af1f679a8a94fb6b40ec31936b668c28e8e16f
Luiz0tavio <me@luizotav.io>
2019-05-05 13:31:40 -0300

    HillClimbing() improvement: allows lateral movement (fn tableau)

--- src/puzzle/solve/Local.hpp
+++ src/puzzle/solve/Local.hpp
@@ -11,21 +11,28 @@ namespace puzzle
     {
         class Local
         {
-            
+
         public:
 
-            static void HillClimbing(std::array<std::array<char, 3>, 3> initial_state)
+            static void HillClimbing(std::array<std::array<char, 3>, 3> initial_state, uint32_t k = 8)
             {
                 std::unique_ptr<Board> next_node = FactoryBoard::create(initial_state, HeuristicsFlags::MANHATTAN_DISTANCE_TO_FINAL_STATE);
 
-                std::cout << "HillClimbing" << std::endl;
+                std::cout << "Hill Climbing" << std::endl;
                 std::cout << next_node->toString("Initial Board") << std::endl;
+
+                uint32_t current_k = k;
 
                 while (true)
                 {
                     std::vector<std::unique_ptr<Board>> moves = next_node->getAllowedMoves();
 
                     uint32_t current_eval = next_node->getHeuristicValue(HeuristicsFlags::MANHATTAN_DISTANCE_TO_FINAL_STATE);
+                    if(current_eval == 0) {
+                        std::cout << "HillClimbing found solution!" << std::endl;
+                        return;
+                    }
+
                     uint32_t next_eval = UINT32_MAX;
 
                     for(auto& move: moves) {
@@ -36,8 +43,10 @@ namespace puzzle
                         }
                     }
 
-                    if(current_eval < next_eval) {
-                        std::cout << "HillClimbing found solution!" << std::endl;
+                    current_k = current_eval == next_eval ? current_k - 1 : k;
+
+                    if(current_eval < next_eval || current_k == 0) {
+                        std::cerr << "HillClimbing cannot find a solution!" << std::endl;
                         return;
                     }
                 }
